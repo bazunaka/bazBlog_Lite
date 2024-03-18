764a339fefa6e4f40643f3ae5329f658a6568bbd
bazunaka-macbook <bazunaka32@gmail.com>
2024-03-18 21:24:06 +0300

    refactor app

--- bazbloglite/src/test/java/ru/bazunaka/bazbloglite/BazblogliteApplicationTests.java
+++ bazbloglite/src/test/java/ru/bazunaka/bazbloglite/BazblogliteApplicationTests.java
@@ -1,13 +1,13 @@
-package ru.bazunaka.bazblog;
+package ru.bazunaka.bazbloglite;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BazblogApplicationTests {
+class BazblogliteApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
