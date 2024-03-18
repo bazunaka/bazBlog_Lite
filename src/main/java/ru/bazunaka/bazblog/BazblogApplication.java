c15b2b7d171e222e7fd2770ec35aa09929b21749
bazunaka-macbook <bazunaka32@gmail.com>
2024-03-18 17:52:08 +0300

    create new project

---
+++ src/main/java/ru/bazunaka/bazblog/BazblogApplication.java
@@ -0,0 +1,13 @@
+package ru.bazunaka.bazblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bazunaka/bazblog/BazblogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bazunaka.bazblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
