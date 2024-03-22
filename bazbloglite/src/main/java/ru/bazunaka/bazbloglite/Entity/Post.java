7031a7919d81d0a331df86333b842ec53359ca10
bazunaka-macbook <bazunaka32@gmail.com>
2024-03-22 12:49:00 +0300

    add new class Post and interface

--- bazbloglite/src/main/java/ru/bazunaka/bazbloglite/Entity/Post.java
+++ bazbloglite/src/main/java/ru/bazunaka/bazbloglite/Entity/Post.java
@@ -6,20 +6,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "users")
-public class User {
+@Table(name = "posts")
+public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
 
-    @Column(name = "full_name")
-    private String fullName;
+    @Column(name = "user_ID")
+    private Long userID;
     @Column(name = "user_name", nullable = false, unique = true)
     private String userName;
     @Column(name = "biography")
---
+++ bazbloglite/src/main/java/ru/bazunaka/bazbloglite/Repository/UserRepository.java
@@ -0,0 +1,8 @@
+package ru.bazunaka.bazbloglite.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.bazunaka.bazbloglite.Entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
