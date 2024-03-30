9323b5db06781e4ff09c49176d8a7a7f2fe0045e
bazunaka <40122285+bazunaka@users.noreply.github.com>
2024-03-30 22:46:37 +0300

    create interface

--- bazblog/src/main/java/ru/bazblog/bazdiary/repository/UserRepository.java
+++ bazblog/src/main/java/ru/bazblog/bazdiary/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package ru.bazblog.bazdiary.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.bazblog.bazdiary.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
     
 }
