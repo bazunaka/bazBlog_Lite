4c823914ebd4dac876dc2ce34bc192988f7a78b7
bazunaka <40122285+bazunaka@users.noreply.github.com>
2024-04-28 16:53:48 +0300

    test

---
+++ bazblog/src/main/java/ru/bazblog/bazdiary/BazblogApplication.java
@@ -0,0 +1,13 @@
+package ru.bazblog.bazdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BazblogApplication.class, args);
+    }
+
+}
