96f56b33e308dfe9032bfcfa6577b5090bfadf12
bazunaka <40122285+bazunaka@users.noreply.github.com>
2024-05-01 23:40:41 +0300

    success logout

--- bazbloglite/src/main/java/ru/bazunaka/bazbloglite/Controllers/MainController.java
+++ bazbloglite/src/main/java/ru/bazunaka/bazbloglite/Controllers/MainController.java
@@ -91,9 +91,9 @@ public class MainController {
     public String login() {
         return "login";
     }
-    @GetMapping("/admin")
+    @GetMapping("/registration")
     public String admin() {
-        return "admin";
+        return "registration";
     }
     @GetMapping("/manager")
     public String manager() {
