db17c06a141f23204f8951cc3a7f3f4ffe1a27d0
bazunaka <40122285+bazunaka@users.noreply.github.com>
2024-05-05 22:51:38 +0300

    refactor sql script

--- bazbloglite/src/main/resources/scripts/create_tables.sql
+++ bazbloglite/src/main/resources/scripts/create_tables.sql
@@ -35,3 +35,19 @@ alter table public.bazblog_user_accounts_roles
 insert into public.bazblog_user_roles(authority)
 values ('ROLE_USER'),
        ('ROLE_ADMIN');
+
+create table public.bazblog_user_profiles
+(
+    id integer primary key ,
+    nickname varchar(32) unique not null ,
+    imageLink varchar(128) not null
+);
+--rollback drop table public.bazblog_user_profiles;
+
+alter table public.bazblog_user_profiles
+    add constraint user_profiles__user_accounts_fk
+        foreign key (id) references public.bazblog_user_accounts (id);
+
+alter table public.bazblog_user_profiles
+    add constraint user_profiles_nickname__unique
+        unique (nickname);
