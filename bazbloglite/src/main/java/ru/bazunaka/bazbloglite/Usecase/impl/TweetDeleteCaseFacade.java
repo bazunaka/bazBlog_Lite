3a53b2b901e600f5efb483673d532ae0843716e8
bazunaka-macbook <bazunaka32@gmail.com>
2024-06-27 23:24:45 +0300

    add func for delete tweet

--- bazbloglite/src/main/java/ru/bazunaka/bazbloglite/Usecase/impl/TweetDeleteCaseFacade.java
+++ bazbloglite/src/main/java/ru/bazunaka/bazbloglite/Usecase/impl/TweetDeleteCaseFacade.java
@@ -1,9 +1,44 @@
 package ru.bazunaka.bazbloglite.Usecase.impl;
 
 import org.springframework.stereotype.Component;
+import ru.bazunaka.bazbloglite.Entity.Tweet;
+import ru.bazunaka.bazbloglite.Entity.UserProfile;
+import ru.bazunaka.bazbloglite.Mapper.TweetEditRequestToTweetMapper;
+import ru.bazunaka.bazbloglite.Mapper.TweetToTweetResponseMapper;
+import ru.bazunaka.bazbloglite.Services.CurrentUserProfileService;
+import ru.bazunaka.bazbloglite.Services.TweetService;
 import ru.bazunaka.bazbloglite.Usecase.TweetDeleteCase;
 
 @Component
 public class TweetDeleteCaseFacade implements TweetDeleteCase {
 
+    private final TweetService tweetService;
+    private final CurrentUserProfileService currentUserProfileService;
+
+    public TweetDeleteCaseFacade(TweetService tweetService, CurrentUserProfileService currentUserProfileService) {
+        this.tweetService = tweetService;
+        this.currentUserProfileService = currentUserProfileService;
+    }
+
+    @Override
+    public void deleteTweet(long tweetId) {
+        UserProfile actor = this.currentUserProfileService
+                .currentUserProfile();
+
+        UserProfile owner = this.tweetService
+                .findTweetById(tweetId)
+                .map(Tweet::getUserProfile)
+                .orElseThrow(() -> {
+                    String errorMessage = String.format("Пост с id = %d не существует", tweetId);
+                    return new RuntimeException(errorMessage);
+                });
+
+        if (!actor.equals(owner)) {
+            String errorMessage = String
+                    .format("Удаление поста с id = %d запрещено. Пользователь %s не является его владельцем",
+                            tweetId,
+                            actor.getNickname());
+            throw new RuntimeException(errorMessage);
+        }
+    }
 }
