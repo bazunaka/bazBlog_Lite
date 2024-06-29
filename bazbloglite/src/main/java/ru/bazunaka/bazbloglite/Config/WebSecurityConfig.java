4cc61c67cca9e1a24d33ee6a8c89a9f4a4a40a7e
bazunaka-macbook <bazunaka32@gmail.com>
2024-06-29 14:55:52 +0300

    add comments

--- bazbloglite/src/main/java/ru/bazunaka/bazbloglite/Config/WebSecurityConfig.java
+++ bazbloglite/src/main/java/ru/bazunaka/bazbloglite/Config/WebSecurityConfig.java
@@ -12,12 +12,30 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * Этот код является конфигурационным классом для Spring Security.
+ * Он определяет правила безопасности и настройки аутентификации.
+ */
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
+    /**
+     * WebSecurityConfig - это конфигурационный класс, помеченный аннотациями @Configuration и @EnableWebSecurity,
+     * который определяет настройки безопасности для веб-приложения.
+     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        /*
+          Это метод, помеченный аннотацией @Bean, который создает цепочку фильтров безопасности.
+          Он определяет правила доступа к различным URL-адресам в приложении.
+         */
+        /*
+         * authorizeHttpRequests - определяет правила авторизации для различных URL-адресов.
+         * В данном случае, /api/v1/accounts/register доступен для всех, /error доступен для всех,
+         * /api/v1/demo/just-role-user доступен только для пользователей с ролью "USER",
+         * /api/v1/demo/just-role-admin доступен только для пользователей с ролью "ADMIN",
+         * а все остальные URL-адреса требуют аутентификации.
+         */
         http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
@@ -33,6 +51,9 @@ public class WebSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
+        /*
+        Создает экземпляр PasswordEncoder, который используется для хеширования паролей пользователей.
+         */
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
