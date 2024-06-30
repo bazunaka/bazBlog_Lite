e9432871fcfdcb274fb1d9ed031b1df124500efb
bazunaka-macbook <bazunaka32@gmail.com>
2024-06-30 23:42:04 +0300

    add comments

--- bazbloglite/src/main/java/ru/bazunaka/bazbloglite/Controllers/UserController.java
+++ bazbloglite/src/main/java/ru/bazunaka/bazbloglite/Controllers/UserController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.*;
 import ru.bazunaka.bazbloglite.Model.RegisterRequest;
 import ru.bazunaka.bazbloglite.Usecase.RegisterUserAccountCase;
 
+/**
+ * Контроллер для управления пользователями.
+ * Обрабатывает HTTP-запросы, связанные с регистрацией пользователей.
+ */
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/accounts")
@@ -14,10 +18,20 @@ public class UserController {
 
     private final RegisterUserAccountCase registerUserAccountCase;
 
+    /**
+     * Конструктор контроллера пользователей.
+     *
+     * @param registerUserAccountCase компонент для регистрации пользователей
+     */
     public UserController(RegisterUserAccountCase registerUserAccountCase) {
         this.registerUserAccountCase = registerUserAccountCase;
     }
 
+    /**
+     * Обрабатывает POST-запрос для регистрации нового пользователя.
+     *
+     * @param registerRequest запрос на регистрацию пользователя
+     */
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
     public void registerUser(@Valid @RequestBody RegisterRequest registerRequest) {
