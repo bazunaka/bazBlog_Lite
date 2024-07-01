e892549af68082f4b0e6143719a1a132a631df7c
bazunaka-macbook <bazunaka32@gmail.com>
2024-07-01 14:36:16 +0300

    add comments

--- bazbloglite/src/main/java/ru/bazunaka/bazbloglite/Controllers/UserProfileController.java
+++ bazbloglite/src/main/java/ru/bazunaka/bazbloglite/Controllers/UserProfileController.java
@@ -6,16 +6,30 @@ import org.springframework.web.bind.annotation.*;
 import ru.bazunaka.bazbloglite.Model.UserProfileRegisterRequest;
 import ru.bazunaka.bazbloglite.Usecase.UserProfileRegisterCase;
 
+/**
+ * Контроллер для управления профилями пользователей.
+ * Обрабатывает HTTP-запросы, связанные с регистрацией профилей пользователей.
+ */
 @RestController
 @RequestMapping("/api/v1/user-profiles")
 public class UserProfileController {
 
     private final UserProfileRegisterCase registerCase;
 
+    /**
+     * Конструктор контроллера профилей пользователей.
+     *
+     * @param registerCase компонент для регистрации профилей пользователей
+     */
     public UserProfileController(UserProfileRegisterCase registerCase) {
         this.registerCase = registerCase;
     }
 
+    /**
+     * Обрабатывает POST-запрос для регистрации нового профиля пользователя.
+     *
+     * @param registerRequest запрос на регистрацию профиля пользователя
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void registerUserProfile(@Valid @RequestBody UserProfileRegisterRequest registerRequest) {
