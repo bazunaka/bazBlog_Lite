4c5c0ce4513af31d5e9548798c13a24ed1aca2d9
bazunaka <40122285+bazunaka@users.noreply.github.com>
2024-07-02 21:38:20 +0300

    add validate subscribe

--- bazbloglite/src/main/java/ru/bazunaka/bazbloglite/Usecase/impl/SubscriptionAddCaseFacade.java
+++ bazbloglite/src/main/java/ru/bazunaka/bazbloglite/Usecase/impl/SubscriptionAddCaseFacade.java
@@ -3,6 +3,7 @@ package ru.bazunaka.bazbloglite.Usecase.impl;
 import org.springframework.stereotype.Component;
 
 import ru.bazunaka.bazbloglite.Entity.Subscription;
+import ru.bazunaka.bazbloglite.Entity.UserProfile;
 import ru.bazunaka.bazbloglite.Mapper.SubscribeRequestToSubscriptionMapper;
 import ru.bazunaka.bazbloglite.Model.SubscribeRequest;
 import ru.bazunaka.bazbloglite.Services.SubscriptionService;
@@ -23,6 +24,17 @@ public class SubscriptionAddCaseFacade implements SubscriptionAddCase {
     @Override
     public void subscribe(SubscribeRequest subscribeRequest) {
         Subscription subscription = this.subscriptionMapper.map(subscribeRequest);
+        UserProfile follower = subscription.getFollower();
+        UserProfile followed = subscription.getFollowed();
+
+        if (follower.equals(followed)) {
+            throw new RuntimeException("Подписка на самого себя не имеет смысла");
+        }
+
+        if (this.subscriptionService.existSubscription(subscription)) {
+            String errorMessage = String.format("Вы уже подписаны на %s", followed.getNickname());
+            throw new RuntimeException(errorMessage);
+        }
 
         this.subscriptionService.createSubscribe(subscription);
     }
